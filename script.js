6d4168e8c790a6fe7d249030880408c0fd41cb8d
enayat789 <enayatbhai2006@gmail.com>
2023-08-20 09:51:58 +0530

    After clicking button page loading will be shown

---
+++ script.js
@@ -0,0 +1,9 @@
+function show_loader() {    
+ const loader_div =   document.querySelector('.loader');
+ loader_div.style.display='flex'
+}
+
+function hide_loader(){
+    const loader_div =   document.querySelector('.loader');
+ loader_div.style.display='none'
+}
